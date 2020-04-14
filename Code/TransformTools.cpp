407d3dbcd0329a4aa9e1d74aac8b05699be91091
Fiction52s <fiction52s@gmail.com>
2020-04-14 08:37:42 -0700

    rotation working

--- Code/TransformTools.cpp
+++ Code/TransformTools.cpp
@@ -1,4 +1,5 @@
 #include "TransformTools.h"
+#include "VectorMath.h"
 
 using namespace std;
 using namespace sf;
@@ -12,6 +13,8 @@ TransformTools::TransformTools()
 
 void TransformTools::Reset(Vector2f &p_center, Vector2f &p_size)
 {
+	mode = NONE;
+
 	center = p_center;
 	size = p_size;
 
@@ -24,6 +27,9 @@ void TransformTools::Reset(Vector2f &p_center, Vector2f &p_size)
 	tRect.setOrigin(tRect.getLocalBounds().width / 2,
 		tRect.getLocalBounds().height / 2);
 	tRect.setPosition(center);
+
+	rotation = 0.f;
+	scale = 1.f;
 }
 
 void TransformTools::Draw(sf::RenderTarget *target)
@@ -33,20 +39,71 @@ void TransformTools::Draw(sf::RenderTarget *target)
 
 void TransformTools::Update( Vector2f &worldPos, bool mouseDown )
 {
-	if (mouseDown)
+	if (!mouseDown)
 	{
-		Vector2f diff = worldPos - center;
-
-		scale = diff.x / (origSize.x / 2.f);
-
-		if (scale < .1)
-			scale = .1;
-
-		size = Vector2f(origSize.x * scale, origSize.y * scale);
-
-		tRect.setSize(size);
-		tRect.setOrigin(tRect.getLocalBounds().width / 2,
-			tRect.getLocalBounds().height / 2);
-		tRect.setPosition(center);
+		mode = NONE;
+		return;
 	}
+
+	switch (mode)
+	{
+	case NONE:
+	{
+		if (mouseDown)
+		{
+			startClick = worldPos;
+			mode = ROTATE;
+			rotationStart = rotation;
+		}
+		break;
+	}
+	case MOVE:
+	{
+
+		break;
+	}
+	case SCALE:
+	{
+		if (mouseDown)
+		{
+			Vector2f diff = worldPos - center;
+
+			scale = diff.x / (origSize.x / 2.f);
+
+			if (scale < .1)
+				scale = .1;
+
+			size = Vector2f(origSize.x * scale, origSize.y * scale);
+
+			tRect.setSize(size);
+			tRect.setOrigin(tRect.getLocalBounds().width / 2,
+				tRect.getLocalBounds().height / 2);
+			tRect.setPosition(center);
+		}
+		break;
+	}
+	case ROTATE:
+	{
+		Vector2f startDir = normalize(startClick - GetRotationAnchor());
+		Vector2f currDir = normalize(worldPos - GetRotationAnchor());
+		
+		float diffRad = GetVectorAngleDiffCW(startDir, currDir);
+		float diffDeg = diffRad / PI * 180;
+
+		rotation = rotationStart + diffDeg;
+
+		tRect.setRotation(rotation);
+		/*tRect.setOrigin(tRect.getLocalBounds().width / 2,
+			tRect.getLocalBounds().height / 2);
+		tRect.setPosition(center);*/
+		
+		break;
+	}
+	}
+	
+}
+
+sf::Vector2f TransformTools::GetRotationAnchor()
+{
+	return center + originOffset;
 }
--- Code/TransformTools.h
+++ Code/TransformTools.h
@@ -5,10 +5,22 @@
 
 struct TransformTools
 {
+	enum Mode
+	{
+		NONE,
+		MOVE,
+		SCALE,
+		ROTATE
+	};
+
+	Mode mode;
+
 	TransformTools();
 	void Reset(sf::Vector2f &p_center, sf::Vector2f &p_size);
 	void Draw(sf::RenderTarget *target);
 	void Update( sf::Vector2f &worldPos, bool mouseDown );
+
+	sf::Vector2f GetRotationAnchor();
 
 	sf::Vector2f center;
 	sf::Vector2f originOffset;
@@ -17,6 +29,9 @@ struct TransformTools
 	sf::Vector2f origCenter;
 	sf::Vector2f origSize;
 
+	sf::Vector2f startClick;
+
+	float rotationStart;
 
 	float scale;
 	float rotation; //degrees
