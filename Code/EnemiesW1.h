c7675d938f08fca41f4b86f235643d0e11ecdb65
Fiction52s <fiction52s@gmail.com>
2020-04-17 06:14:33 -0700

    removed files from project

---
+++ Code/EnemiesW1.h
@@ -0,0 +1,26 @@
+#ifndef __ENEMIESW1_H__
+#define __ENEMIESW1_H__
+
+
+#include "Enemy_Airdasher.h"
+#include "Enemy_BasicEffect.h"
+#include "Enemy_BasicTurret.h"
+#include "Enemy_Blocker.h"
+#include "Enemy_Booster.h"
+#include "Enemy_Comboer.h"
+#include "Enemy_Crawler.h"
+#include "Enemy_CrawlerQueen.h"
+#include "Enemy_FootTrap.h"
+#include "Enemy_Goal.h"
+#include "Enemy_Patroller.h"
+#include "Enemy_RaceFightTarget.h"
+#include "Enemy_Shard.h"
+#include "Enemy_Shroom.h"
+#include "Enemy_SplitComboer.h"
+#include "Enemy_Spring.h"
+#include "GroundTrigger.h"
+//#include "Enemy_Juggler.h"
+#include "FlowerPod.h"
+
+
+#endif
